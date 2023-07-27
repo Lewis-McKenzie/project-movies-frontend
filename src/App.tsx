4cc859f3d5f584a39ce9c3800b52b6021dc0e655
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-07-27 19:00:58 +0100

    Trailers added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import './App.css';
 import api from './api/axiosConfig';
 import React from "react";
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
 
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -21,10 +26,17 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  })
+  }, [])
 
   return (
     <div className="App">
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home movies={movies}/>}></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+type Movie = {
+    poster: string,
+    title: string,
+    backdrops: any,
+    trailerLink: string,
+}
+
+type Movies = {
+    movies: Movie[]
+}
+
+interface MovieCardStyle {
+    '--img': string;
+}
+
+const Hero = ({movies}: Movies) => {
+  return (
+    <div className='movie-carousel-container'>
+        <Carousel>
+            {
+                movies.map((movie: Movie) => {
+                    const backgroundImage = movie.backdrops && movie.backdrops.length > 0 ? `url(${movie.backdrops[0]})` : 'none';
+          
+                    // Cast the inline style object to the defined MovieCardStyle
+                    const cardStyle = { '--img': backgroundImage } as React.CSSProperties;
+          
+                    return (
+                        <Paper>
+                            <div className='movie-card-container'>
+                                <div className="movie-card" style={cardStyle}>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt="" />
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                        <div className='movie-buttons-container'>
+                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}/>
+                                            <div className="play-button-icon-container">
+                                                <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Paper>
+                    )
+                })
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Hero
